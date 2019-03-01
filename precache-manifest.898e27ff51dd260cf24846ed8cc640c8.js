9e850e1918e9955ce96d673189d840e4006ab829
Madhumankatha <madhumankatha@gmail.com>
2019-03-01 16:52:54 +0530

    Updates

---
+++ precache-manifest.898e27ff51dd260cf24846ed8cc640c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-Axios-MaterialUi/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "358d18a5aa51e21b5162",
+    "url": "/React-Axios-MaterialUi/static/js/runtime~main.358d18a5.js"
+  },
+  {
+    "revision": "70000aa90549d76df516",
+    "url": "/React-Axios-MaterialUi/static/js/main.70000aa9.chunk.js"
+  },
+  {
+    "revision": "553e6bc6158dbb1833bf",
+    "url": "/React-Axios-MaterialUi/static/js/2.553e6bc6.chunk.js"
+  },
+  {
+    "revision": "70000aa90549d76df516",
+    "url": "/React-Axios-MaterialUi/static/css/main.6687c7d2.chunk.css"
+  },
+  {
+    "revision": "3f6815dcb4f11750de05f4ac84caaf32",
+    "url": "/React-Axios-MaterialUi/index.html"
+  }
+];
